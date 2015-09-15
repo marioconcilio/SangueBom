db3708fa26dd19fa0d09790d7af3223fb8db3ddd
mconcilio <marioconcilio@gmail.com>
2015-09-15 14:01:12 -0300

    Initial Commit

---
+++ SangueBom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SangueBom
+//
+//  Created by Mario Concilio on 9/15/15.
+//  Copyright (c) 2015 Mario Concilio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SangueBom/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SangueBom
+//
+//  Created by Mario Concilio on 9/15/15.
+//  Copyright (c) 2015 Mario Concilio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
